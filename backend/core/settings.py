76f25a31d5f15276afc70452a7388353002d01f1
Liam <herryson5896@gmail.com>
2025-10-27 22:11:11 -0400

    fix: update template path

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -147,7 +147,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend_build'),
     ]
-# TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
