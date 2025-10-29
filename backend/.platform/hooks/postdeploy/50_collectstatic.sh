1e0713d4e698c0a1ee4694ad74aad9a0f2b1dde6
Liam <herryson5896@gmail.com>
2025-10-29 05:12:55 -0400

    ensure postdeploy hooks are executable

--- backend/.platform/hooks/postdeploy/50_collectstatic.sh
+++ backend/.platform/hooks/postdeploy/50_collectstatic.sh
@@ -7,6 +7,5 @@ export DJANGO_SETTINGS_MODULE=core.settings
 
 echo "[postdeploy] Running collectstatic"
 
-#
 /var/app/venv/*/bin/python manage.py collectstatic --noinput
 echo "[postdeploy] collectstatic done."
