5798aaccdea5ecbcf0cd28494ce738376d4333f7
Liam <herryson5896@gmail.com>
2025-10-29 20:16:59 -0400

    Edit build directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path";
-// https://vite.dev/config/
+import path from "path"
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // production or development
+  base: isProduction ? '/static/' : '/',
+  
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  build: {
+    // Output dir：Put build files into ../backend/static
+    outDir: path.resolve(__dirname, "../backend/frontend_build"),
+    emptyOutDir: true, // Empty before build
+  },
 })
