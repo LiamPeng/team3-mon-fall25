a54a5c18b573d35a7f04bc2bcfc996352752f5dd
Liam <herryson5896@gmail.com>
2025-10-30 04:19:02 -0400

    DEBUG=True

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,7 +41,7 @@ if not SECRET_KEY:
             env_file.write(f"DJANGO_SECRET_KEY={SECRET_KEY}\n")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = [
 #     "127.0.0.1",
